3900dca7b9985a0a6ed27eb2f61fb31ddcc2a39e
Priyam Gupta <priyam.gupta@abspl.in>
2017-07-18 14:53:14 +0530

    dynamodb-added

--- handler.js
+++ handler.js
@@ -1,16 +1,57 @@
 'use strict';
 
+const AWS = require('aws-sdk');
+const docClient = new AWS.DynamoDB.DocumentClient({region : 'us-east-1'});
+
 module.exports.hello = (event, context, callback) => {
-  if(event.httpMethod === "GET" && event.queryStringParameters && event.queryStringParameters.name) {
+  if(event.httpMethod === "GET") {
+    if(event.pathParameters && event.pathParameters.userId) {
+      callback(null, {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: 'GET Request with path param',
+          input: event
+        })
+      });
+    }
+    if(event.queryStringParameters && event.queryStringParameters.name) {
+      callback(null, {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: 'Hola ' + event.queryStringParameters.name + ', Welcome to the Serverless Architecture Domain.',
+        })
+      });
+    }
     callback(null, {
       statusCode: 200,
       body: JSON.stringify({
-        message: 'Hola ' + event.queryStringParameters.name + ', Welcome to the Serverless Architecture Domain.',
+        message: 'Invalid GET Request',
       })
     });
   }
 
   if(event.httpMethod === "POST" && event.body) {
+    let json = JSON.parse(event.body);
+    let params = {
+      Items : json,
+      TableName : 'user'
+    }
+    docClient.put(params, function(err, data) {
+      if(err)
+      callback(err, null)
+      if(data)
+      callback(null, data)
+    })
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'Inserting of data was unsuccessful.',
+        object: json
+      })
+    });
+  }
+
+  if(event.httpMethod === "DELETE") {
     let json = JSON.parse(event.body);
     callback(null, {
       statusCode: 200,
